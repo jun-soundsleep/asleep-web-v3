e18b03125918ec3e45ccb1540043a9f6d73e4885
namjunpark <nnamjun94@gmail.com>
2021-08-18 15:49:25 +0900

    초기 세팅 완료

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,41 @@
 import "../../styles/globals.css";
+import { Global, css } from "@emotion/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+          }
+          a {
+            text-decoration: none;
+            color: inherit;
+          }
+          img {
+            max-width: 100%;
+          }
+          button,
+          input {
+            outline: 0;
+            border: 0;
+            background: none;
+          }
+          ol,
+          ul {
+            list-style: none;
+          }
+          html,
+          body {
+            width: 100%;
+            height: 100%;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
---
+++ styles/styles.js
@@ -0,0 +1,6 @@
+import { css, jsx } from "@emotion/react";
+
+const divStyle = css`
+  background-color: hotpink;
+  font-size: 24px;
+`;
