51f63333501c4fd208a0470da68a980a5c928528
namjunpark <nnamjun94@gmail.com>
2021-08-18 14:13:18 +0900

    first setting

---
+++ hooks/userWindowSize.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      function handleResize() {
+        setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }
+  }, []);
+};
---
+++ styles/device.js
@@ -0,0 +1,15 @@
+export const size = {
+  mobile: "360px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1536px",
+  desktop: "1920px",
+};
+
+export const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
