908bf395fdaaaef88e6fbf38d1ef07cff55afa0e
Jun Park <nnamjun94@gmail.com>
2022-11-28 16:24:57 +0900

    test: api routes

---
+++ src/pages/api/ces/index.ts
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  console.log(req.query);
+  try {
+    if (req.query.test === '1') {
+      res.redirect('https://asleep.ai');
+    } else if (req.query.test === '2') {
+      res.redirect('https://asleep.ai/test');
+    }
+    res.status(200).json({ name: 'John Doe' });
+  } catch (e) {
+    console.error(e);
+  }
+}
