f8f8f5de9215968353a20fd6757fa293d6517472
Jun Park <nnamjun94@gmail.com>
2022-11-28 18:07:00 +0900

    test: api routes -2

--- src/pages/api/ces/index.ts
+++ src/pages/api/ces/index.ts
@@ -2,11 +2,11 @@ export default function handler(req, res) {
   console.log(req.query);
   try {
     if (req.query.test === '1') {
-      res.redirect('https://asleep.ai');
+      return res.redirect('https://asleep.ai');
     } else if (req.query.test === '2') {
-      res.redirect('https://www.naver.com');
+      return res.redirect('https://www.naver.com');
     }
-    res.status(200).json({ name: 'John Doe' });
+    return res.status(200).json({ name: 'John Doe' });
   } catch (e) {
     console.error(e);
   }
