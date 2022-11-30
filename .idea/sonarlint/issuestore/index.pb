7f7c46f71be9491087be3f4189b38b5877c6958a
Jun-soundsleep <soundsleep@asleep.ai>
2022-11-30 17:16:42 +0900

    feat: change api routes to typescript

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,4 @@
 
-:
-
-.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
 T
 $.idea/codeStyles/codeStyleConfig.xml,d/d/dd7b3350c29c49497756c7f32c69a3bed18b1cba
 L
--- src/pages/api/ces/index.ts
+++ src/pages/api/ces/index.ts
@@ -1,13 +1,39 @@
 export default function handler(req, res) {
-  console.log(req.query);
   try {
-    if (req.query.test === '1') {
-      return res.redirect('https://asleep.ai');
-    } else if (req.query.test === '2') {
-      return res.redirect('https://www.rudderstack.com/');
+    switch (req.query.redirection) {
+      case '0':
+        return res.redirect('https://asleep.ai');
+      case '1':
+        return res.redirect('https://toss.im/');
+      case '2':
+        return res.redirect('https://baemin.com/');
+      case '3':
+        return res.redirect('https://line.me/ko/');
+      case '4':
+        return res.redirect('https://www.kakaocorp.com/page/');
+      case '5':
+        return res.redirect(
+          'https://www.google.com/webhp?hl=ko&sa=X&ved=0ahUKEwj45oTxsNX7AhUNmFYBHSEoBzQQPAgI'
+        );
+      case '6':
+        return res.redirect('https://en.dict.naver.com/#/main');
+      case '7':
+        return res.redirect(
+          'https://papago.naver.com/?sk=auto&tk=ko&hn=0&st=Use%20the%20JavaScript%20documentation%20to%20find%20a%20method%20on%20the%20built-in%20Number%20object%20that%20checks%20if%20a%20number%20is%20an%20integer.'
+        );
+      case '8':
+        return res.redirect(
+          'https://www.figma.com/file/DvI7vbVrMUQv3oJjYgtJkS/Epic%3A-Report?node-id=1%3A15589'
+        );
+      case '9':
+        return res.redirect('https://www.asleep.ai/business/');
+      case '10':
+        return res.redirect('https://asleep.ai');
+      default:
+        return res.redirect('https://www.asleep.ai/');
     }
-    return res.status(200).json({ name: 'John Doe' });
   } catch (e) {
     console.error(e);
+    return res.redirect('https://asleep.ai');
   }
 }
