e5186063ade0b40a2004f71634b930c6413a745f
Jun-soundsleep <soundsleep@asleep.ai>
2022-12-12 16:20:18 +0900

    feat: add ces alert animation

--- src/v3-component/layout/Navigation/CesAlert.tsx
+++ src/v3-component/layout/Navigation/CesAlert.tsx
@@ -1,23 +1,42 @@
 import SubStrackIcon from '/public/imagev3/icon/Subtract.svg';
+import styles from './ces.module.css';
+import useGetCurrentSize from '../../../../hooks/useGetCurrentSize';
+import { deviceType } from '../../../../styles/device';
 
 const CesAlert = () => {
-  const textStyle = `b2-bold-small text-white medium:b1-bold-medi large:sub-large`;
+  const currentSize = useGetCurrentSize();
+  const isSmall = currentSize === deviceType.small;
 
   return (
     <div
-      className={`bg-[#191998] h-[38px] w-screen px-[16px] flex items-center justify-center medium:h-[46px] overflow-hidden`}
+      className={`relative bg-[#191998] h-[38px] w-screen flex items-center justify-center overflow-hidden medium:h-[46px]`}
     >
-      <div className={`flex items-center justify-between  gap-[15px]`}>
-        <div className={textStyle}>ASLEEP in 2023 CES</div>
-        <div
-          className={`w-[16px] h-[16px] medium:w-[26px] medium:h-[26px] large:w-[32px] large:h-[32px]`}
-        >
-          <SubStrackIcon className={`large:w-[32px] large:h-[32px]`} />
-        </div>
-        <div className={textStyle}>Look around our booth</div>
+      <div
+        className={`flex gap-[16px] w-full h-full absolute left-0 top-0 ${styles.scrollAnimation} medium:px-[36px] large:px-[316px]`}
+      >
+        <Item />
+        {isSmall && <Item />}
       </div>
     </div>
   );
 };
 
 export default CesAlert;
+
+const Item = () => {
+  const textStyle = `flex-shrink-0 b2-bold-small text-white medium:b1-bold-medi large:sub-large`;
+
+  return (
+    <div
+      className={`flex flex-shrink-0 items-center justify-between w-full h-full`}
+    >
+      <div className={textStyle}>ASLEEP in 2023 CES</div>
+      <div
+        className={`flex-shrink-0 w-[16px] h-[16px] medium:w-[26px] medium:h-[26px] large:w-[32px] large:h-[32px]`}
+      >
+        <SubStrackIcon className={`large:w-[32px] large:h-[32px]`} />
+      </div>
+      <div className={textStyle}>Look around our booth</div>
+    </div>
+  );
+};
